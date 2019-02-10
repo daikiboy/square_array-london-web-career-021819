8589574ab946113e314d0f6ef3dd7df025d20f8b
simone.rainieri <simone.rainieri@gmail.com>
2019-02-10 15:36:46 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,5 +1,8 @@
 def square_array(array)
   array1 = []
   array.each do |x| 
-    array2 = # your code here
+    array2 = x ** 2 
+    array1 << array2
+    end
+    return array1# your code here
 end
